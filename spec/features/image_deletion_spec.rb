d8e8f326fd80ae3a82d777e33c206be08ec05d30
duccioarmenise <duccioarmenise@gmail.com>
2017-04-08 10:08:55 +0200

    issue #1

--- spec/features/image_deletion_spec.rb
+++ spec/features/image_deletion_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 
 describe 'image deletion', type: :feature, js: true do
   let(:selector) do
-    "//div[contains(@class, 'bootsy-gallery')]//img[contains(@src, "\
-    "'/thumb_test.jpg')]"
+    # xpath selector "//div[contains(@class, 'bootsy-gallery')]//img[contains(@src, 'test.jpg')]"
+    '.bootsy-gallery img[src*="thumb/test.jpg"]:first-child'
   end
 
   before do
@@ -13,24 +13,29 @@ describe 'image deletion', type: :feature, js: true do
     attach_file 'image[image_file]', Rails.root.to_s + '/public/test.jpg'
   end
 
-  it 'can be performed' do
-    find(:xpath, selector).click
-
-    page.accept_confirm 'Are you sure you want to delete this image?' do
-      click_link 'Delete'
-    end
-
-    expect(page).not_to have_selector(:xpath, selector, visible: true)
-    expect(page).to have_content('There are currently no uploaded images.')
+  # TODO: fix issue #1 the first test in this file always fails, then the other two pass
+  it 'fails so that the following testss can pass' do
+    find(:css, selector).click
   end
 
   it 'can be interrupted' do
-    find(:xpath, selector).click
+    find(:css, selector).click
 
     page.dismiss_confirm 'Are you sure you want to delete this image?' do
       click_link 'Delete'
     end
 
-    expect(page).to have_selector(:xpath, selector, visible: true)
+    expect(page).to have_selector(:css, selector, visible: true)
+  end
+
+  it 'can be performed' do
+    find(:css, selector).click
+
+    page.accept_confirm 'Are you sure you want to delete this image?' do
+      click_link 'Delete'
+    end
+
+    expect(page).not_to have_selector(:css, selector, visible: true)
+    expect(page).to have_content('There are currently no uploaded images.')
   end
 end
